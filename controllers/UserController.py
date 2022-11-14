f79ef6a6e6e3447ae92df94bb258379504c28281
Nguyenle23 <beenbin2308@gmail.com>
2022-11-15 01:34:39 +0700

    Build | Full CRUD with User

--- controllers/UserController.py
+++ controllers/UserController.py
@@ -1,71 +1,76 @@
 
+import datetime
+import json
+from bson import json_util, ObjectId
 from bson.json_util import dumps
-from bson.objectid import ObjectId
-from pymongo import MongoClient
-from config import DATABASENAME, MONGO_URI, USER_COLLECTION
 from flask import jsonify, request
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_bcrypt import Bcrypt
 
 from controllers.ErrorController import not_found
+from database.Connection import Connection
 
-client = MongoClient(MONGO_URI)
-db = client[DATABASENAME]
-collection = db[USER_COLLECTION]
+class UserController:
+  def createUser():
+    if request.method == 'POST':
+      data = request.json
+      
+      data['avatar'] = ''
+      data['createdAt'] = datetime.datetime.utcnow()
+      data['updatedAt'] = datetime.datetime.utcnow()
 
-def add_user():
-    _json = request.json
-    _name = _json['name']
-    _email = _json['email']
-    _password = _json['pwd']
+      if (len(data['password']) <= 6):
+        return jsonify('Password must be at least 6 characters long')
+      else:
+        data['password'] = Bcrypt().generate_password_hash(data['password']).decode('utf-8')
+        checkEmailExist = Connection.db().users.find_one({'email': data.get('email')})
+        if (checkEmailExist == None):
+          Connection.db().users.insert_one(data)
+          resp = json.loads(json_util.dumps(data))
+          return {
+            'message': "User added successfully!",
+            'data': [resp]
+          }
+        else:
+          resp = jsonify("User already exists!")
+          resp.status_code = 409
+          return resp
 
-    # validate the received values
-    if _name and _email and _password and request.method == 'POST':
-        
-        # do not save password as a plain text
-        _hashed_password = generate_password_hash(_password)
-    
-        # save details
-        id = db.users._insert_one({'name': _name, 'email': _email, 'pwd': _hashed_password})
-        resp = jsonify("User added successfully!")
-        resp.status_code = 200
+  def getALlUsers():
+    users = Connection.db().users.find()
+    resp = dumps(users)
+    return resp
+
+  def getUserByID(userID):
+      user = Connection.db().users.find_one({'_id': ObjectId(userID)})
+      resp = dumps(user)
+      if (resp == 'null'):
+        return not_found(userID)
+      else:
         return resp
-    # else:
-    #     return not_found()
 
-def getALlUsers():
-  users = db.users.find()
-  resp = dumps(users)
-  return resp
+  def updateUserByID(userID):
+    data = request.json
+    data['updatedAt'] = datetime.datetime.utcnow()
 
-def getUserByID(userID):
-    user = db.users.find_one({'_id': ObjectId(userID)})
-    resp = dumps(user)
-    if (resp == 'null'):
-      return not_found(userID)
+    if (len(data['password']) <= 6):
+      return jsonify('Password must be at least 6 characters long')
     else:
-      return resp
+      data['password'] = Bcrypt().generate_password_hash(data['password']).decode('utf-8')
+      checkUser = Connection.db().users.update_one({'_id': ObjectId(userID)}, {'$set': data})
+      if (checkUser.modified_count == 0):
+        return not_found(userID)
+      else:
+        resp = json.loads(json_util.dumps(data))
+        return {
+          'message': "User updated successfully!",
+          'data': [resp]
+        }
 
-def delete_user(id):
-  db.users.delete_one({'_id': ObjectId(id)})
-  resp = jsonify("User deleted successfully!")
-  resp.status_code = 200
-  return resp
-
-def update_user():
-  _id = request.json['_id']
-  _name = request.json['name']
-  _email = request.json['email']
-  _password = request.json['pwd']
-
-  # validate the received values
-  if _name and _email and _password and _id and request.method == 'PUT':
-      # do not save password as a plain text
-      _hashed_password = generate_password_hash(_password)
-  
-      # save edits
-      db.users.update_one({'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)}, {'$set': {'name': _name, 'email': _email, 'pwd': _hashed_password}})
-      resp = jsonify("User updated successfully!")
+  def deleteUserByID(userID):
+    checkUser = Connection.db().users.delete_one({'_id': ObjectId(userID)})
+    if (checkUser.deleted_count == 0):
+        return not_found(userID)
+    else:
+      resp = jsonify("User deleted successfully!")
       resp.status_code = 200
       return resp
-  # else:
-  #     return not_found()
--- routes/UserRouter.py
+++ routes/UserRouter.py
@@ -1,9 +1,11 @@
 from flask import Blueprint
 
-from controllers.UserController import getALlUsers, getUserByID
+from controllers.UserController import UserController
 UserRouter = Blueprint('UserRouter', __name__)
 
-UserRouter.route('/', methods=['GET'])(getALlUsers)
-UserRouter.route('/<userID>', methods=['GET'])(getUserByID)
-
+UserRouter.route('/', methods=['POST'])(UserController.createUser)
+UserRouter.route('/', methods=['GET'])(UserController.getALlUsers)
+UserRouter.route('/<userID>', methods=['GET'])(UserController.getUserByID)
+UserRouter.route('/<userID>', methods=['PUT'])(UserController.updateUserByID)
+UserRouter.route('/<userID>', methods=['DELETE'])(UserController.deleteUserByID)
 
